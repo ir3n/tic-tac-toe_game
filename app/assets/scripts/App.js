32d4c4df03955e09196cf0d9e32dde12faa2d7b8
ir3n <iren.borada@gmail.com>
2019-04-05 22:08:32 +0300

    Basic logic of who wins and update roundscore.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
+import { equal } from "assert";
 
-const cells = $("td");
 let highScoreInput = $("input#highscore");
 
 let players = [];
@@ -14,7 +14,6 @@ function createPlayersArray(numOfPlayers) {
   for (let i = 0; i < numOfPlayers; i++) {
     players.push({
       player: i + 1,
-      buttonsClicked: [],
       score: 0,
       winner: false
     });
@@ -37,60 +36,91 @@ $(".grid-btn").each(function() {
 });
 
 //EVENT LISTENERS
-cells.each(function() {
+$("td").each(function() {
   $(this).click(e => handleBtnClick(e));
 });
 
 function handleBtnClick(e) {
+  //set clicked button's layout
   handleClickedBtnLayout(e.target);
 
-  let x = e.currentTarget.cellIndex;
-  let y = e.currentTarget.parentElement.rowIndex;
-
-  //compare with existing
-  let sameXCounter = 1;
-  let sameYCounter = 1;
-  let sameDCounter = 0;
-  if (x === y || x === 3 - y - 1) {
-    sameDCounter++;
+  //check if we have round winner
+  if (
+    checkRowIfWon(curPlayer) ||
+    checkColIfWon(curPlayer) ||
+    checkDiag1IfWon(curPlayer) ||
+    checkDiag2IfWon(curPlayer)
+  ) {
+    updateScore(curPlayer);
   }
-
-  let buttonsClickedArr = players[curPlayer - 1].buttonsClicked;
-  for (let i = 0; i < buttonsClickedArr.length; i++) {
-    if (x === y || x === 3 - y - 1) {
-      sameDCounter++;
-    }
-    if (buttonsClickedArr[i].y === y) {
-      sameYCounter++;
-    }
-    if (buttonsClickedArr[i].x === x) {
-      sameXCounter++;
-    }
-    //save clicked buttons for each player
-
-    //check if we have round winner
-    checkWinner(sameXCounter, sameYCounter, sameDCounter, curPlayer);
-    //start over
-  }
-  buttonsClickedArr.push({
-    y: e.currentTarget.parentElement.rowIndex,
-    x: e.currentTarget.cellIndex
-  });
-
   togglePlayer();
-  console.log(sameXCounter, sameYCounter, sameDCounter);
   console.log(players);
 }
 
-function checkWinner(x, y, d, player) {
-  if (x === 3 || y === 3 || d === 3) {
-    players[player - 1].score++;
-    $(`#player-${player}-score`).html(`${players[player - 1].score}`);
+//1. check if current player has 3 cells in a row
+function checkRowIfWon(player) {
+  let curPlayerWon = false;
+  $("tr").each(function() {
+    //find cells with same class
+    let cells = [...$(this).find(`td[class="${player}"]`)];
+    if (cells.length === 3) {
+      curPlayerWon = true;
+    }
+  });
+  return curPlayerWon;
+}
+//2. check if current player has 3 cells with same index
+function checkColIfWon(player) {
+  let curPlayerWon = false;
+  for (let i = 0; i < 3; i++) {
+    let cells = [
+      //we make an array with te cells that have the class of the player and the same order in the row
+      ...$("tr").find(`td[class="${player}"]:nth-of-type(${i + 1})`)
+    ];
+    if (cells.length === 3) {
+      curPlayerWon = true;
+    }
   }
+  return curPlayerWon;
+}
+//3. check diagonal \
+function checkDiag1IfWon(player) {
+  let curPlayerWon = false;
+  let cells = [];
+  $("tr").each(function(i) {
+    //we make an array with the tds that have the same index with the row and the class of the player
+    cells.push(...$(this).find(`td:eq('${i}')[class="${player}"]`));
+    if (cells.length === 3) {
+      curPlayerWon = true;
+    }
+  });
+  return curPlayerWon;
+}
+//4. check diagonal /
+function checkDiag2IfWon(player) {
+  let curPlayerWon = false;
+  let cells = [];
+  //we start from the last row and look for the tds whose index follows the type 3-rowIndex-1
+  for (let i = 2; i >= 0; i--) {
+    cells.push(
+      ...$(`tr:eq('${i}')`).find(`td:eq('${3 - i - 1}')[class="${player}"]`)
+    );
+    if (cells.length === 3) {
+      curPlayerWon = true;
+    }
+  }
+  return curPlayerWon;
+}
+
+function updateScore(player) {
+  players[curPlayer - 1].score++;
+  $(`#player-${player}-score`).html(`${players[player - 1].score}`);
 }
 
 function handleClickedBtnLayout(btn) {
   $(btn).addClass(`player-${curPlayer}--clicked`);
+  //add class to the cell in order to recognize it when we check for the winner
+  btn.parentElement.classList.add(`${curPlayer}`);
   $(btn).prop("disabled", true);
   $(btn).prepend(`<img class="player-${curPlayer}-saber"
   src="./assets/images/${curPlayer}-lightsaber.png"
